9d54c810d58090aa544a6b23d0084d0c267b6b19
deepakyec <deep3265@gmail.com>
2018-03-06 18:49:00 +0530

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      title: 'Welcome to Your About Page'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
